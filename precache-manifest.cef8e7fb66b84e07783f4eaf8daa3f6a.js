c597f8319cbc47f89578c7e0655abb3b0646b63d
Kevlar2222 <kevin.satti@hotmail.co.uk>
2020-07-29 15:37:30 +0800

    Updates

---
+++ precache-manifest.cef8e7fb66b84e07783f4eaf8daa3f6a.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97944f01999bf6b5c931a85c0f6879a9",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "40feb1caa1e10fb9aca2",
+    "url": "/weather-app/static/css/main.a201fb10.chunk.css"
+  },
+  {
+    "revision": "e70a16919314eb86f903",
+    "url": "/weather-app/static/js/2.87b1354f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.87b1354f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40feb1caa1e10fb9aca2",
+    "url": "/weather-app/static/js/main.571dadcd.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "8e69716c4b72e97bdd3f561093c7d8d9",
+    "url": "/weather-app/static/media/clear.8e69716c.jpg"
+  },
+  {
+    "revision": "1478c006bbb3dc0fbf208315016ce7f9",
+    "url": "/weather-app/static/media/clouds.1478c006.jpg"
+  },
+  {
+    "revision": "ade49d8d4ca18f2fd7dc64dc0c9e72ae",
+    "url": "/weather-app/static/media/rain.ade49d8d.jpg"
+  },
+  {
+    "revision": "c809d74e8de13102b51dceda8842b679",
+    "url": "/weather-app/static/media/snow.c809d74e.jpg"
+  },
+  {
+    "revision": "de75e0d698122c1c1b40e7ff7f1e989d",
+    "url": "/weather-app/static/media/storm.de75e0d6.jpg"
+  }
+]);
